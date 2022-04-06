c41b87764c7ae6bd1d3f27c1c78475438395c200
조한슬 <jhs4015@kaist.ac.kr>
2022-04-07 01:14:51 +0900

    models.py & utils.py 재업

--- Hanseul/models.py
+++ Hanseul/models.py
@@ -3,7 +3,6 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
 from .utils import make_one_hot
 
 
--- Hanseul/utils.py
+++ Hanseul/utils.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn.functional as F
 
+
 def make_one_hot(x):
     if type(x) is not torch.Tensor:
         x = torch.LongTensor(x)
